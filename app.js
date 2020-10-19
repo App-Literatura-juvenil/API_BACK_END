504f47d392869a5908266cda400ff25233826857
AlejoWilches0112 <cawilchesfe@poligran.edu.co>
2020-10-19 12:09:31 -0500

    Implementación de cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/author.route');
@@ -12,6 +13,7 @@ const dbManager = require("./modules/database/db.manager");
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
